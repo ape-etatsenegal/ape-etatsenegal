076828160d71ede15159701361293b9c60159c4e
apeetatsenegal <apeetatsenegal@gmail.com>
2025-09-29 11:23:02 +0000

    MAJ 9

--- app/cadre/page.tsx
+++ app/cadre/page.tsx
@@ -25,8 +25,26 @@ export default function Page() {
           </video>
         </div>
       </section>
+      <div className="border-t border-gray-300 mt-3 pt-3 pb-3 mb-3"></div>
+      {/* Hero Section */}
+
+            <section className="w-full flex justify-center items-center mt-8 mb-12 px-4">
+        <div className="relative w-full md:w-3/4 lg:w-2/3 aspect-video rounded-2xl overflow-hidden shadow-lg">
+          <video
+            controls
+            autoPlay={false}
+            loop
+            muted
+            className="w-full h-full object-cover"
+          >
+            <source src="/videos/v1.mp4" type="video/mp4" />
+            Votre navigateur ne supporte pas la lecture de vidéos.
+          </video>
+        </div>
+      </section>
       <div className="border-t border-gray-300 mt-3 pt-3"></div>
       {/* Hero Section */}
+
       <section className="relative w-full mb-16 mt-4 md:mt-0">
         {/* Image responsive */}
         <Image
