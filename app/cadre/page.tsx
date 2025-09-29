cbc2e9332b13839e5b8362c4730c73e603b51390
apeetatsenegal <apeetatsenegal@gmail.com>
2025-09-29 11:17:45 +0000

    MAJ 8

--- app/cadre/page.tsx
+++ app/cadre/page.tsx
@@ -5,11 +5,27 @@ import Header from "@/components/ui/Header";
 import Footer from "@/components/ui/Footer";
 
 export default function Page() {
-
   return (
     <div className="min-h-screen mx-auto bg-gray-50">
       {/* Header */}
-        <Header />
+      <Header />
+      {/* Hero Section */}
+
+      <section className="w-full flex justify-center items-center mt-8 mb-12 px-4">
+        <div className="relative w-full md:w-3/4 lg:w-2/3 aspect-video rounded-2xl overflow-hidden shadow-lg">
+          <video
+            controls
+            autoPlay={false}
+            loop
+            muted
+            className="w-full h-full object-cover"
+          >
+            <source src="/videos/v2.mp4" type="video/mp4" />
+            Votre navigateur ne supporte pas la lecture de vidéos.
+          </video>
+        </div>
+      </section>
+      <div className="border-t border-gray-300 mt-3 pt-3"></div>
       {/* Hero Section */}
       <section className="relative w-full mb-16 mt-4 md:mt-0">
         {/* Image responsive */}
@@ -24,7 +40,7 @@ export default function Page() {
       </section>
 
       {/* Footer */}
-     <Footer />
+      <Footer />
     </div>
   );
 }
