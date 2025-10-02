f023898cbac9c1fed762527e4c7b2efd5eb79eb7
apeetatsenegal <apeetatsenegal@gmail.com>
2025-10-02 16:48:27 +0000

    MAJ 22

--- next.config.js
+++ next.config.js
@@ -1,21 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Ignore les erreurs ESLint lors de la build
   eslint: {
     ignoreDuringBuilds: true,
   },
-
-  // Pour désactiver l’optimisation automatique des images
   images: {
     unoptimized: true,
   },
-
-  // Build autonome
   output: 'standalone',
-
-  // Experimental options (vide pour éviter les warnings)
-  experimental: {
-    // serverActions supprimé pour compatibilité Next.js 15+
+  experimental: {},
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'ape-etatsenegal.com',
+          },
+        ],
+        destination: 'https://www.ape-etatsenegal.com/:path*',
+        permanent: true,
+      },
+    ];
   },
 };
 
